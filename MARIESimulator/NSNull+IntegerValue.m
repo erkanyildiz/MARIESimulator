aa3c45bb51fd81df0e941226e67762785499ec89
Erkan YILDIZ <erkanyildiz@me.com>
2014-05-16 12:35:52 +0300

    Added NSNull integerValue category

---
+++ MARIESimulator/NSNull+IntegerValue.m
@@ -0,0 +1,16 @@
+//
+//  NSNull+IntegerValue.m
+//  MARIESimulator
+//
+//  Created by Erkan YILDIZ on 16/05/14.
+//  Copyright (c) 2014 InnovationBox. All rights reserved.
+//
+
+#import "NSNull+IntegerValue.h"
+
+@implementation NSNull (IntegerValue)
+-(NSInteger)integerValue
+{
+    return 0;
+}
+@end
